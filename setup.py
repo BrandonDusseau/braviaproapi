ebb8d6888d51b18ca369ba8410c83e88e3a1b0ba
Brandon Dusseau <brandon.dusseau@gmail.com>
2019-10-01 22:32:17 -0400

    Update project metadata

--- setup.py
+++ setup.py
@@ -5,22 +5,27 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="braviaproapi",
-    version="0.0.1",
+    version="0.9.0",
     author="Brandon Dusseau",
-    author_email="brandon.dusseau@gmail.com",
-    description="Library for communicating with Sony Bravia TVs utilizing its Bravia Professional API.",
+    author_email="brandon.dusseau+pypi@gmail.com",
+    description="Library for controlling Sony Bravia TVs utilizing their Bravia Professional API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BrandonDusseau/py-bravia-pro-api",
     packages=setuptools.find_packages(exclude=["http"]),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent ",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    install_requires=["requests", "python-dateutil", "packaging", "pycryptodome"],
+    install_requires=[
+        "requests>=2,<3",
+        "python-dateutil>=2,<3",
+        "packaging>=19",
+        "pycryptodome>=3,<4"
+    ],
     keywords='sony bravia television remote control library'
 )
